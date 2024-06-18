f60ebf5727f7143494ae9a7e7dc8dcc103a3e57f
juarezmaxi8@gmail.com <juarezmaxi8@gmail.com>
2024-06-18 19:30:29 -0300

    PMT Previo a lib2

--- PMT/src/main.cpp
+++ PMT/src/main.cpp
@@ -43,10 +43,12 @@ std::string modelSaver(double& m, double& b) {
         std::cout << "Error al abrir el archivo models.csv" << std::endl;
         // throw std::invalid_argument("Error al abrir el archivo models.csv"); // HACER TRY CATCH
     }
-    std::cout << "models.csv abierto exitosamente" << std::endl;
+    // std::cout << "models.csv abierto exitosamente" << std::endl;
 
     modelSaver.seekp(0, std::ios_base::end);
     modelSaver << modelName << "," << m << "," << b << std::endl;
+
+    std::cout << "Su modelo " << modelName << " se guardo correctamente" << std::endl;
 
     modelSaver.close();
 
@@ -100,8 +102,38 @@ void predOption(double& m, double& b) {
     }
 }
 
-int main() {
+void modelOption() {
     std::string fileName;
+    std::cout << "Ingrese el nombre del archivo con datos (incluya el .csv)" << std::endl;
+    std::cin >> fileName;
+    std::string filePath = "csv/";
+    filePath.append(fileName);
+
+    readData readData(filePath);
+    readData.openFile();
+
+    std::vector<dataStruct> data;
+    readData.read(data);
+
+    size_t dataSize = data.size();
+    double* xValues = new double[dataSize];
+    double* yValues = new double[dataSize];
+
+    for (size_t i = 0; i < dataSize; ++i) {
+        xValues[i] = data[i].x;
+        yValues[i] = data[i].y;
+    }
+
+    // for (const auto& dataStruct : data) {
+    //     std::cout << "X: " << dataStruct.x << ", Y: " << dataStruct.y << std::endl;
+    // }
+
+    simpleLinearRegression(xValues, yValues, dataSize, ecVars.m, ecVars.b);
+
+    std::cout << "m: " << ecVars.m << " b: " << ecVars.b << std::endl;
+}
+
+int main() {
     int choice = 0;
 
     std::cout << "Para cargar un modelo ingrese 1 | Para crear un nuevo modelo ingrese 2" << std::endl;
@@ -111,62 +143,19 @@ int main() {
         modelLoader();
         predOption(ecVars.m, ecVars.b);
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // std::cout << "Ingrese el nombre del archivo con datos (incluya el .csv)" << std::endl;
-    // std::cin >> fileName;
-    // std::string filePath = "csv/";
-    // filePath.append(fileName);
-
-    // readData readData(filePath);
-    // readData.openFile();
-
-    // std::vector<dataStruct> data;
-    // readData.read(data);
-
-    // size_t dataSize = data.size();
-    // double* xValues = new double[dataSize];
-    // double* yValues = new double[dataSize];
-
-    // for (size_t i = 0; i < dataSize; ++i) {
-    //     xValues[i] = data[i].x;
-    //     yValues[i] = data[i].y;
-    // }
-
-    // ------------------------------- PREDICCION
-
-
-
-    // std::string modelName = modelSaver(m, b);
-
-    // std::cout << "m: " << m << "\nb: " << b << std::endl;
-
-    // std::cout << "\nIngrese un valor de x para predecir: " << std::endl;
-    // std::cin >> predValue;
-
-    // double yPred = m * predValue + b;
-
-    // std::cout << "El valor de Y predicho es: " << yPred << std::endl;
-
-    // for (const auto& dataStruct : data) {
-    //     std::cout << "X: " << dataStruct.x << ", Y: " << dataStruct.y << std::endl;
-    // }
+    else if (choice == 2) {
+        modelOption();
+        modelSaver(ecVars.m, ecVars.b);
+    }
+    else {
+        while (true) {
+            std::cout << "Opcion inexistente, ingrese nuevamente la operacion" << std::endl;
+            std::cin >> choice;
+            if (choice == 1 || choice == 2) {
+                break;
+            }
+        }
+    }
 
     system("pause");
 
